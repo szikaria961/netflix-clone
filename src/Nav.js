0030837e1d4b2fdb102670875aba7effe6bfbbdf
szikaria961 <szikaria961@gmail.com>
2021-02-05 13:31:12 -0600

    Add navbar component with styles

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netlix Logo"
+      />
+      <img
+        className="nav-avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Avatar Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav
